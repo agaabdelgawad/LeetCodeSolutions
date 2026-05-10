75d82f76332022c5bb04f2d54e922ac3b4ea3375
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-05-10 12:46:20 +0000

    Sync LeetCode submission - Two Sum - Runtime - 6 ms (46.80%), Memory - 49.3 MB (65.11%)

--- LeetCodeSolutions/0001-two-sum/solution.cs
+++ LeetCodeSolutions/0001-two-sum/solution.cs
@@ -1,17 +1,16 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-     Dictionary<int,int> map = new Dictionary<int,int>();
-     
-     for (int i = 0 ; i < nums.Length ; i++)
-     {
-        int complement = target - nums[i];
+        HashSet<int> complements = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++){
+            int currentComp = target - nums[i];
 
-        if(map.ContainsKey(complement))
-            return new int[2]{map[complement], i};
+            if (complements.Contains(currentComp)){
+                return new int[]{Array.IndexOf(nums, currentComp), i};
+            }
 
-        map[nums[i]]= i;
-     }
+            complements.Add(nums[i]);
+        }
 
-     return new int[2]{0,0};
+        return new int[]{-1, -1};
     }
 }
