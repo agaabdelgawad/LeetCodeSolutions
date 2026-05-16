bcef5c4cd069eef864d5a98b5897f90acb494185
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-05-16 15:22:07 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array - Runtime - 0 ms (100.00%), Memory - 51 MB (75.86%)

---
+++ LeetCodeSolutions/0026-remove-duplicates-from-sorted-array/solution.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int i = 1;
+        int j = 1;
+
+        while (i < nums.Length){
+            if (nums[i] != nums[i-1]){
+                nums[j] = nums[i];
+
+                j++;
+            }
+
+            i++;
+        }
+
+        return j;
+    }
+}
