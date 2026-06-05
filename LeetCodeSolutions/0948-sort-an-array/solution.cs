fac12dbfa076477cc869a5168edc396caef3dcea
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-06-05 15:58:46 +0000

    Sync LeetCode submission - Sort an Array - Runtime - 54 ms (92.56%), Memory - 82.1 MB (81.88%)

--- LeetCodeSolutions/0948-sort-an-array/solution.cs
+++ LeetCodeSolutions/0948-sort-an-array/solution.cs
@@ -1,29 +1,50 @@
-public class Solution {
-    public int[] SortArray(int[] nums) {
-        int[] result = new int[nums.Length];
-        Dictionary<int, int> numCounts = new Dictionary<int, int>();
-        int i = 0;
-        int minValue = int.MaxValue;
-        int maxValue = int.MinValue;
+public class Solution{
+    public int[] SortArray(int[] nums){
+        QuickSort(nums, 0, nums.Length -1);
+        return nums;
+    }
 
-        for(i = 0; i < nums.Length; i++){
-            numCounts[nums[i]] = numCounts.GetValueOrDefault(nums[i]) + 1;
+    private void QuickSort(int[] nums, int left, int right){
+        if(left >= right) return;
 
-            minValue = Math.Min(nums[i], minValue);
-            maxValue = Math.Max(nums[i], maxValue);
+        if(right == left + 1){
+            if(nums[left] > nums[right]) Swap(nums, left, right);
+            return;
         }
 
-        i = 0;
-        for(int curValue = minValue; curValue <= maxValue; curValue++){
-            if(!numCounts.ContainsKey(curValue)){
-                continue;
-            }
+        int pivot = Partition(nums, left, right);
 
-            while(numCounts[curValue]-- > 0){
-                result[i++] = curValue;
-            }
+        QuickSort(nums, left, pivot - 1);
+        QuickSort(nums, pivot + 1, right);
+    }
+
+    private int Partition(int[] nums, int left, int right){
+        int med = left + ((right - left) / 2);
+
+        if(nums[left] > nums[right]) Swap(nums, left, right);
+        if(nums[left] > nums[med]) Swap(nums, left, med);
+        if(nums[med] > nums[right]) Swap(nums, med, right);
+
+        Swap(nums, med, right - 1);
+
+        int i = left; int j = right - 1;
+
+        while(true){
+            while(nums[++i] < nums[right - 1]);
+            while(nums[--j] > nums[right - 1]);
+
+            if(i >= j) break;
+
+            Swap(nums, i, j);
         }
 
-        return result;
+        Swap(nums, i, right - 1);
+        return i;
+    }
+
+    private void Swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
