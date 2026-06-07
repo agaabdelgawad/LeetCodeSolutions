49897bbf860a50aa92a6017fbd03aaf759383835
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-06-07 15:27:39 +0000

    Sync LeetCode submission - Container With Most Water - Runtime - 1 ms (99.70%), Memory - 61.8 MB (36.99%)

--- LeetCodeSolutions/0011-container-with-most-water/solution.cs
+++ LeetCodeSolutions/0011-container-with-most-water/solution.cs
@@ -1,18 +1,13 @@
 public class Solution {
     public int MaxArea(int[] height) {
+        int i = 0; int j = height.Length - 1;
         int area = 0;
-        int i = 0;
-        int j = height.Length - 1;
 
-        while(i <  j){
+        while (i < j){
             area = Math.Max(area, Math.Min(height[i], height[j]) * (j - i));
 
-            if(height[i] <= height[j]){
-                i++;
-            }
-            else{
-                j--;
-            }
+            if(height[i] <= height[j]) i++;
+            else j--;
         }
 
         return area;
