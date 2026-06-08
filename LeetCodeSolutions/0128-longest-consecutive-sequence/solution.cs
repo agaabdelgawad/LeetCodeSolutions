9116eee9c33b96379be622676a7bef7430b914bd
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-06-08 15:30:59 +0000

    Sync LeetCode submission - Longest Consecutive Sequence - Runtime - 29 ms (54.92%), Memory - 72.5 MB (72.43%)

--- LeetCodeSolutions/0128-longest-consecutive-sequence/solution.cs
+++ LeetCodeSolutions/0128-longest-consecutive-sequence/solution.cs
@@ -1,26 +1,17 @@
 public class Solution {
     public int LongestConsecutive(int[] nums) {
-       if(nums.Length == 0){
-            return 0;
-        }
+        if(nums.Length == 0) return 0;
 
-        HashSet<int> uniqueSet = new HashSet<int>(nums);
-        int result = 1;
-        int temp = 1;
+        HashSet<int> numSet = new HashSet<int>(nums);
+        int longestStreak = 1;
+
+        foreach (int num in numSet) {
+            if (numSet.Contains(num - 1)) continue;
+            int currentStreak = 1;
+            while (numSet.Contains(num + currentStreak)) currentStreak++;
+            if (currentStreak > longestStreak) longestStreak = currentStreak;
+        }
         
-        foreach(int num in uniqueSet){
-            if(uniqueSet.Contains(num - 1)){
-                continue;
-            }
-
-            while(uniqueSet.Contains(num + temp)){
-                temp++;
-            }
-
-            result = Math.Max(result, temp);
-            temp = 1;
-        }
-
-        return result;
+        return longestStreak;
     }
 }
