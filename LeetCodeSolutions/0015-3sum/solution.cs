73d061f12c8cab5e4ceec57d25d255ba886e95f8
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-06-09 17:56:44 +0000

    Sync LeetCode submission - 3Sum - Runtime - 32 ms (90.44%), Memory - 66.4 MB (26.41%)

--- LeetCodeSolutions/0015-3sum/solution.cs
+++ LeetCodeSolutions/0015-3sum/solution.cs
@@ -1,48 +1,30 @@
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {
         Array.Sort(nums);
-
-        int i; int j; int sum;
         IList<IList<int>> result = new List<IList<int>>();
+        int n = nums.Length;
 
-        for(int k = 0; k < nums.Length; k++){
-            if(nums[k] > 0){
-                break;
-            }
+        for(int k = 0; k < n - 2; k++){
+            if(nums[k] > 0) break;
+            if(k != 0 && nums[k] == nums[k - 1]) continue;
             
-            if(k != 0 && nums[k] == nums[k - 1]){
-                continue;
-            }
-            
-            i = k + 1;
-            j = nums.Length - 1;
-            sum = 0;
+            int i = k + 1; int j = n - 1;
 
             while(i < j){
-                sum = nums[k] + nums[i] + nums[j];
+                int sum = nums[k] + nums[i] + nums[j];
 
-                if(sum < 0){
-                    i++;
-                }
-                else if(sum > 0){
-                    j--;
-                }
+                if(sum < 0) i++;
+                else if(sum > 0) j--;
                 else{
                     result.Add(new List<int>{nums[k], nums[i], nums[j]});
                     i++;
                     j--;
 
-                    while(i < j && nums[i] == nums[i - 1]){
-                        i++;
-                    }
-                    
-                    while(i < j && nums[j] == nums[j + 1]){
-                        j--;
-                    }
+                    while(i < j && nums[i] == nums[i - 1]) i++;
                 }
             }
         }
 
-        return result;
+        return result; 
     }
 }
