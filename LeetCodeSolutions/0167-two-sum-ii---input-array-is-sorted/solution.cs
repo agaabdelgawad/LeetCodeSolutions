007b39a58b3bc8af656347f7dedc5378ad8eb42f
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-06-09 16:39:35 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted - Runtime - 0 ms (100.00%), Memory - 49.1 MB (95.57%)

--- LeetCodeSolutions/0167-two-sum-ii---input-array-is-sorted/solution.cs
+++ LeetCodeSolutions/0167-two-sum-ii---input-array-is-sorted/solution.cs
@@ -1,23 +1,15 @@
 public class Solution {
     public int[] TwoSum(int[] numbers, int target) {
-        int i = 0;
-        int j = numbers.Length - 1;
-        int sum = 0;
+        int i = 0; int j = numbers.Length - 1;
 
         while(i < j){
-            sum = numbers[i] + numbers[j];
+            int sum = numbers[i] + numbers[j];
 
-            if(sum > target){
-                j--;
-            }
-            else if(sum < target){
-                i++;
-            }
-            else{
-                return new int[2]{i + 1, j + 1};
-            }
+            if(sum > target) j--;
+            else if(sum < target) i++;
+            else return new int[]{i + 1, j + 1};
         }
 
-        return new int[2]{-1, -1};
+        return new int[]{-1, -1};
     }
 }
