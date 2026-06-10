58b91cc0df44781813d514a477a79041c3f07bfc
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-06-10 13:27:46 +0000

    Sync LeetCode submission - Rotate Array - Runtime - 0 ms (100.00%), Memory - 64.8 MB (55.28%)

--- LeetCodeSolutions/0189-rotate-array/solution.cs
+++ LeetCodeSolutions/0189-rotate-array/solution.cs
@@ -1,9 +1,20 @@
 public class Solution {
     public void Rotate(int[] nums, int k) {
-       int r = k % nums.Length;
+        int n = nums.Length;
+        k %= n;
 
-        Array.Reverse(nums);
-        Array.Reverse(nums, 0, r);
-        Array.Reverse(nums, r, nums.Length - r); 
+        Reverse(nums, 0, n - 1);
+        Reverse(nums, 0, k - 1);
+        Reverse(nums, k, n - 1);
+    }
+
+    private void Reverse(int[] nums, int left, int right){
+        while(left < right){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
     }
 }
