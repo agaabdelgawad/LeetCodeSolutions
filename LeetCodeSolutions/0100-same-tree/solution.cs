fd15ced3baabaf770fcc98a145b36b6684c2ab2d
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-07-07 15:16:34 +0000

    Sync LeetCode submission - Same Tree - Runtime - 0 ms (100.00%), Memory - 43.5 MB (32.31%)

--- LeetCodeSolutions/0100-same-tree/solution.cs
+++ LeetCodeSolutions/0100-same-tree/solution.cs
@@ -14,9 +14,7 @@
 public class Solution {
     public bool IsSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null) return true;
-        if(p != null && q != null && p.val == q.val)
-            return (IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right));
-
-        return false;
+        if(p == null || q == null || p.val != q.val) return false;
+        return (IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right));
     }
 }
