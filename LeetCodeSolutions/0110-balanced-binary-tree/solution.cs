7bd11f25b8bcfbb911abf3e0380f69ce9af1ea67
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-07-08 15:47:49 +0000

    Sync LeetCode submission - Balanced Binary Tree - Runtime - 0 ms (100.00%), Memory - 46.9 MB (64.50%)

--- LeetCodeSolutions/0110-balanced-binary-tree/solution.cs
+++ LeetCodeSolutions/0110-balanced-binary-tree/solution.cs
@@ -12,21 +12,17 @@
  * }
  */
 public class Solution {
-    public bool IsBalanced(TreeNode root) {
-        return CalculateDepthIfBalanced(root) != -1;
-    }
+    public bool IsBalanced(TreeNode root) => CalculateDepthIfBalanced(root) != -1;
 
     private int CalculateDepthIfBalanced(TreeNode root) {
         if(root == null) return 0;
 
         int leftDepth = CalculateDepthIfBalanced(root.left);
         if(leftDepth == -1) return -1;
-
         int rightDepth = CalculateDepthIfBalanced(root.right);
         if(rightDepth == -1) return -1;
 
         if(Math.Abs(leftDepth - rightDepth) > 1) return -1;
-
         return 1 + Math.Max(leftDepth, rightDepth);
     }
 }
