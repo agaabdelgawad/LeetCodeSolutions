48cf79a8e5331b956231be5fece7801575ec48c8
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-07-08 15:14:16 +0000

    Sync LeetCode submission - Subtree of Another Tree - Runtime - 2 ms (96.35%), Memory - 48.5 MB (82.98%)

--- LeetCodeSolutions/0572-subtree-of-another-tree/solution.cs
+++ LeetCodeSolutions/0572-subtree-of-another-tree/solution.cs
@@ -23,10 +23,7 @@ public class Solution {
 
     private bool IsSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null) return true;
-        if(p == null || q == null) return false;
-
-        return  p.val == q.val &&
-                IsSameTree(p.left, q.left) &&
-                IsSameTree(p.right, q.right);
+        if(p == null || q == null || p.val != q.val) return false;
+        return (IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right));
     }
 }
