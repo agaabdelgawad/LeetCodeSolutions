36cab31da2d424ef14dd08c9d6b14119c2d1bb19
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-07-10 14:16:52 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Search Tree - Runtime - 99 ms (83.67%), Memory - 50.4 MB (7.86%)

---
+++ LeetCodeSolutions/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.cs
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode current = root;
+
+        while(current != null){
+            if(p.val < current.val && q.val < current.val) current = current.left;
+            else if(p.val > current.val && q.val > current.val) current = current.right;
+            else return current;
+        }
+
+        return null;
+    }
+}
