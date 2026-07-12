84fb7c7ca4f4f62b30f8083cc25d8f37cc7f5c16
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-07-12 16:04:01 +0000

    Sync LeetCode submission - Delete Node in a BST - Runtime - 0 ms (100.00%), Memory - 51.4 MB (94.80%)

---
+++ LeetCodeSolutions/0450-delete-node-in-a-bst/solution.cs
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode DeleteNode(TreeNode root, int key) {
+        if(root == null) return null;
+
+        if (key < root.val) root.left = DeleteNode(root.left, key);
+        else if (key > root.val) root.right = DeleteNode(root.right, key);
+        else{
+            if(root.left == null) return root.right;
+            if(root.right == null) return root.left;
+
+            TreeNode minRight = root.right;
+            while(minRight.left != null) minRight = minRight.left;
+            minRight.left = root.left;
+            return root.right;
+        }
+
+        return root;
+    }
+}
