b4912a0669d33920d79ee469a9325341eea0dae4
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-07-17 15:48:29 +0000

    Sync LeetCode submission - Decode String - Runtime - 1 ms (92.54%), Memory - 40.1 MB (28.36%)

--- LeetCodeSolutions/0394-decode-string/solution.cs
+++ LeetCodeSolutions/0394-decode-string/solution.cs
@@ -2,31 +2,26 @@ public class Solution {
     public string DecodeString(string s) {
         Stack<string> stringStack = new Stack<string>();
         Stack<int> countStack = new Stack<int>();
-        string current = "";
+        StringBuilder current = new StringBuilder();
         int k = 0;
 
-        foreach(char c in s){
-            if(char.IsDigit(c)) k = k * 10 + (c - '0');
+        foreach(var c in s){
+            if(char.IsDigit(c)) k = k * 10 + c - '0';
             else if(c == '['){
-                stringStack.Push(current);
+                stringStack.Push(current.ToString());
                 countStack.Push(k);
-                current = "";
+                current.Clear();
                 k = 0;
             }
             else if(c == ']'){
-                string temp = current;
-                current = stringStack.Pop();
+                string temp = current.ToString();
                 int count = countStack.Pop();
-                while(count > 0){
-                    current += temp;
-                    count --;
-                }
+                current = new StringBuilder(stringStack.Pop());
+                while(count-- > 0) current.Append(temp);
             }
-            else{
-                current += c;
-            }
+            else current.Append(c);
         }
 
-        return current;
+        return current.ToString();
     }
 }
