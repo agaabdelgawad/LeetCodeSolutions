f797896deeb5a36ec27594ebd815edab978845a4
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-08-03 17:07:21 +0000

    Sync LeetCode submission - Jump Game - Runtime - 1 ms (100.00%), Memory - 57.8 MB (68.85%)

---
+++ LeetCodeSolutions/0055-jump-game/solution.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool CanJump(int[] nums) {
+        int n = nums.Length;
+        int goal = n - 1;
+
+        for(int i = n - 2; i >= 0; i--) if(i + nums[i] >= goal) goal = i;
+
+        return goal == 0;
+    }
+}
+
