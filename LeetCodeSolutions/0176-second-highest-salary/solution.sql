c80befb7c355c62bdb3e9260b73a594a6fbe2006
Ahmed Gamal Abdel Gawad <aGa.abdelgawad@gmail.com>
2026-08-16 14:44:47 +0000

    Sync LeetCode submission - Second Highest Salary - Runtime - 570 ms (9.90%), Memory - 0.0B (100.00%)

---
+++ LeetCodeSolutions/0176-second-highest-salary/solution.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+SELECT (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    OFFSET 1 ROW
+    FETCH NEXT 1 ROW ONLY
+) AS SecondHighestSalary
